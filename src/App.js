e88f0cfed9be1a7983c83c88fad72c9c736499d1
stefansnaerh <stefansnaerh@gmail.com>
2022-11-24 15:17:10 +0000

    doing the guides page

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <div className="App">
        <AuthProvider>
         <Navbar />
-        <ModulesPage />
+       <EachModulepage/>
         
      </AuthProvider>
     </div>
