ef8187d98669be8d4dfbcb3eadf8caf53657fa29
Elsa Katrín <elsa.katrin@gmail.com>
2022-11-29 20:25:38 +0000

    hovereffect over images

--- src/components/Gallery/gallery.js
+++ src/components/Gallery/gallery.js
@@ -15,11 +15,19 @@ const Gallery = ()=>{
     }
     getData();
   },[])
-
+//The image that appears when there's no image or error with the image 
   const skipImage = (e)=>{
     e.target.src="https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
   }
   
+const [isHovering, setIsHovering] = useState(false);
+const handleMouseOver = () => {
+    setIsHovering(true);
+};
+const handleMouseOut = () => {
+    setIsHovering(false);
+}
+
   return (
     
     <div className="main-container">
@@ -30,17 +38,25 @@ const Gallery = ()=>{
 
        {projects.map(project=>{
         return (
-          <div className="innerclass" key={project._id}>
+            <>
+            <div onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className="project">
+          <div key={project._id}>
             <img onError={skipImage} className="image" src={project.imageOrGif}></img>
-            <p className="name">{project.sender.name}</p>
-          
           </div>
+          </div>
+          {isHovering && (
+                     <div>
+          <p className="name">{project.sender.name}</p>
+          </div>
+          )}
+ 
+          </>
         )
        })}
 
       </div>
       </div>
-
+            
     
   );
 }
