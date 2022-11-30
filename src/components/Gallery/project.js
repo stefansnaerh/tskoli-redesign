1b7390839a979b28890b584d8f08c52d9c13fe64
Elsa Katrín <elsa.katrin@gmail.com>
2022-11-30 16:33:02 +0000

    linked images to github links

--- src/components/Gallery/project.js
+++ src/components/Gallery/project.js
@@ -10,11 +10,14 @@ const Project = ({project}) => {
     const handleMouseOut = () => {
         setIsHovering(false);
     };
-    
+    console.log(project.url)
     //The image that appears when there's no image or error with the image 
   const skipImage = (e)=>{
     e.target.src="https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
   }
+  const openInNewTab = url => {
+    window.open(url, '_blank', 'noopener, noreferrer');
+  };
 
     return (
         <>
@@ -22,12 +25,17 @@ const Project = ({project}) => {
         <div key={project._id} className="project">
 
             <div className="image"onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
-                <img onError={skipImage} className="image" src={project.imageOrGif}></img>
+                <img onError={skipImage} onClick={()=> openInNewTab (project.url)} className="image" src={project.imageOrGif}></img>
             </div>
             
+
             {isHovering && (
-                    <p className="name">{project.sender.name}</p>        
+                <div className="projectinfo">
+                    <p className="projecttitle">{project.assignment.Title}</p>
+                    <p className="studentname">{project.sender.name}</p> 
+                </div>       
                 )}
+                
          </div>
     </>
     )
