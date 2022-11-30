9d58c45637a1ba2fee793a29b1be5b27016015e4
Elsa Katrín <elsa.katrin@gmail.com>
2022-11-30 13:31:51 +0000

    hover effect and grids

--- src/components/Gallery/gallery.js
+++ src/components/Gallery/gallery.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import './Gallery.scss';
+import Project from  './project' ;
 
 const Gallery = ()=>{
 
@@ -15,42 +16,24 @@ const Gallery = ()=>{
     }
     getData();
   },[])
-//The image that appears when there's no image or error with the image 
-  const skipImage = (e)=>{
-    e.target.src="https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-  }
-  
-const [isHovering, setIsHovering] = useState(false);
-const handleMouseOver = () => {
-    setIsHovering(true);
-};
-const handleMouseOut = () => {
-    setIsHovering(false);
-}
 
+  
   return (
     
     <div className="main-container">
       {/* <p className="subheader">Projects from students 2022</p> */}
       <div className="gallery-container">
-
+        <div className="gallerytxt">
       <h1 className="gallerytxt">{"{ Gallery }"}</h1>
-
+        </div>
        {projects.map(project=>{
-        return (
-            <>
-            <div onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className="project">
-          <div key={project._id}>
-            <img onError={skipImage} className="image" src={project.imageOrGif}></img>
-          </div>
-          </div>
-          {isHovering && (
-                     <div>
-          <p className="name">{project.sender.name}</p>
-          </div>
-          )}
  
-          </>
+        return (
+
+           <Project project={project}>
+
+           </Project>
+
         )
        })}
 
